69e0e52246b0fb9c436e24cda4c208c003714917
jojodev77 <jonathandelaosa@gmail.com>
2019-05-19 19:00:27 +0200

    menu, matgrid, obj user

--- src/app/amterial/material/material.module.ts
+++ src/app/amterial/material/material.module.ts
@@ -3,19 +3,24 @@ import { CommonModule } from '@angular/common';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatGridListModule,
+    MatCardModule
   ],
   exports: [
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatGridListModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
