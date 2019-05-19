e67b1dacab2c0f528389a2c7f3d7f0f2d15c15f7
jojodev77 <jonathandelaosa@gmail.com>
2019-05-19 19:20:58 +0200

    module auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,21 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MenuComponent } from './menu/menu/menu.component';
 import { MaterialModule } from './amterial/material/material.module';
+import { ConnectComponent } from './authentification/connect/connect.component';
+import { CreateUserComponent } from './authentification/create-user/create-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    ConnectComponent,
+    CreateUserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
+    AuthModule,
     BrowserAnimationsModule
   ],
   providers: [],
